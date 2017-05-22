f385fef1d325c9cb253c12548f070b9dcc8469cb
zhang28602 <zeyu930213@gmail.com>
2017-05-22 20:07:41 +0800

    项目初始化

---
+++ ZZYQRCodeSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZZYQRCodeSwift
+//
+//  Created by 张泽宇 on 2017/5/22.
+//  Copyright © 2017年 zzy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ZZYQRCodeSwiftTests/ZZYQRCodeSwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  ZZYQRCodeSwiftTests.swift
+//  ZZYQRCodeSwiftTests
+//
+//  Created by 张泽宇 on 2017/5/22.
+//  Copyright © 2017年 zzy. All rights reserved.
+//
+
+import XCTest
+@testable import ZZYQRCodeSwift
+
+class ZZYQRCodeSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
