d383c888f0553113b6be047c760498147e1d0d0f
zhang28602 <zeyu930213@gmail.com>
2017-05-24 02:13:45 +0800

    修改文件目录

---
+++ ZZYQRCodeSwift/ZZYQRCodeSwift/AVCaptureSessionManager.swift
@@ -0,0 +1,282 @@
+//
+//  AVCaptureSessionManager.swift
+//  ZZYQRCodeSwift
+//
+//  Created by 张泽宇 on 2017/5/23.
+//  Copyright © 2017年 zzy. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import Photos
+
+typealias SuccessBlock = (String?) -> Void
+typealias GrantBlock = () -> ()
+typealias DeniedBlock = () -> ()
+
+class AVCaptureSessionManager: AVCaptureSession, AVCaptureMetadataOutputObjectsDelegate {
+    
+    /// 音效名称
+    var soundName:String?
+    /// 是否播放音效
+    var isPlaySound = false
+    
+    private var block: SuccessBlock?
+    
+    private lazy var device: AVCaptureDevice = {
+       return AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+    }()
+    
+    private lazy var preViewLayer: AVCaptureVideoPreviewLayer = {
+        return AVCaptureVideoPreviewLayer(session: self)
+    }()
+    
+    
+   /// 创建sessionManager
+   ///
+   /// - Parameters:
+   ///   - captureType: 需要扫描的类型
+   ///   - scanRect: 扫描区域这里的Rect(x,y,w,h)分别的取值范围都是0-1 如果需要全屏传入React.null
+   ///   - success: 成功回调
+   convenience init(captureType: AVCaptureType,
+                    scanRect: CGRect,
+                    success: @escaping SuccessBlock) {
+        self.init()
+        block = success
+    
+        var input: AVCaptureDeviceInput?
+        do {
+            input = try AVCaptureDeviceInput(device: device)
+        } catch let error as NSError {
+            print("AVCaputreDeviceError \(error)")
+        }
+        
+        let output = AVCaptureMetadataOutput()
+        output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        
+        if !scanRect.equalTo(CGRect.null) {
+            output.rectOfInterest = scanRect
+        }
+        
+        sessionPreset = AVCaptureSessionPresetHigh
+        if canAddInput(input) {
+            addInput(input)
+        }
+        
+        if canAddOutput(output) {
+            addOutput(output)
+        }
+        output.metadataObjectTypes = captureType.supportTypes()
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(stop),
+                                               name: NSNotification.Name.UIApplicationDidEnterBackground,
+                                               object: nil)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(start),
+                                               name: NSNotification.Name.UIApplicationWillEnterForeground,
+                                               object: nil)
+    
+    }
+    
+    
+    /// 创建sessionManager
+    ///
+    /// - Parameters:
+    ///   - captureType: 需要扫描的类型
+    ///   - scanRect: 扫描区域这里的Rect(x,y,w,h)分别的取值范围都是0-1 如果需要全屏传入React.null
+    ///   - success: 成功回调
+    /// - Returns: manager
+    class func createSessionManager(captureType: AVCaptureType,
+                                    scanRect: CGRect,
+                                    success: @escaping SuccessBlock) ->AVCaptureSessionManager {
+        let result = AVCaptureSessionManager(captureType: captureType, scanRect: scanRect, success: success);
+        return result
+    }
+    
+
+    /// 监测相机权限
+    ///
+    /// - Parameters:
+    ///   - grant: 同意回调
+    ///   - denied: 拒绝回调
+    class func checkAuthorizationStatusForCamera(grant:@escaping GrantBlock, denied:DeniedBlock) {
+        
+        if let device = AVCaptureDevice.devices(withMediaType: AVMediaTypeVideo) {
+            let status = AVCaptureDevice.authorizationStatus(forMediaType: AVMediaTypeVideo)
+            switch status {
+            case .notDetermined:
+                AVCaptureDevice.requestAccess(forMediaType: AVMediaTypeVideo, completionHandler: { (granted) in
+                    if granted {
+                        DispatchQueue.main.async(execute: {
+                            grant()
+                        })
+                    }
+                })
+            case .authorized:
+                grant()
+            case .denied:
+                denied()
+            default:
+                break
+            }
+        }
+    }
+    
+    /// 监测相册权限
+    ///
+    /// - Parameters:
+    ///   - grant: 同意回调
+    ///   - denied: 拒绝回调
+    class func checkAuthorizationStatusForPhotoLibrary(grant:@escaping GrantBlock, denied:DeniedBlock) {
+        let status = PHPhotoLibrary.authorizationStatus()
+        switch status {
+        case .notDetermined:
+            PHPhotoLibrary.requestAuthorization({ (status) in
+                if status == PHAuthorizationStatus.authorized {
+                    DispatchQueue.main.async(execute: {
+                        grant()
+                    })
+                }
+            })
+            
+        case .authorized:
+            grant()
+        case .denied:
+            denied()
+        default:
+            break
+        }
+    }
+    
+    /// 扫描相册中的二维码
+    ///
+    /// - Parameters:
+    ///   - image: 图片
+    ///   - success: 成功回调
+    func scanPhoto(image: UIImage, success: SuccessBlock) {
+        let detector = CIDetector(ofType: CIDetectorTypeQRCode,
+                                  context: nil,
+                                  options: [CIDetectorAccuracy : CIDetectorAccuracyHigh])
+        if detector != nil {
+            let features = detector!.features(in: CIImage(cgImage: image.cgImage!))
+            for temp in features {
+                let result = (temp as! CIQRCodeFeature).messageString
+                success(result)
+                return
+            }
+            success(nil)
+        }else {
+            success(nil)
+        }
+        
+    }
+    
+    /// 显示扫描
+    ///
+    /// - Parameter view: 需要在哪个View中显示
+    func showPreViewLayerIn(view :UIView) {
+        preViewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        preViewLayer.frame = view.bounds
+        view.layer.insertSublayer(preViewLayer, at: 0)
+        start()
+    }
+    
+    
+    /// 开关闪光灯
+    ///
+    /// - Parameter state: 闪光灯状态
+    func turnTorch(state:Bool) {
+        if (device.hasTorch) {
+            do {
+                try device.lockForConfiguration()
+            } catch let error as NSError {
+                print("TorchError  \(error)")
+            }
+            if (state) {
+                device.torchMode = AVCaptureTorchMode.on
+            } else {
+                device.torchMode = AVCaptureTorchMode.off
+            }
+            device.unlockForConfiguration()
+        }
+    }
+    
+    
+    /// 播放音效
+    func playSound() {
+        if isPlaySound {
+            var result = "ZZYQRCode.bundle/sound.caf"
+            if let temp = soundName, temp != ""{
+                result = temp
+            }
+            
+            if let urlstr = Bundle.main.path(forResource: result, ofType: nil) {
+                let fileURL = NSURL(fileURLWithPath: urlstr)
+                var soundID:SystemSoundID = 0;
+                AudioServicesCreateSystemSoundID(fileURL, &soundID)
+                AudioServicesPlaySystemSound(soundID)
+            }
+        }
+    }
+    
+    /// 开启扫描
+    func start() {
+        startRunning()
+    }
+    
+    /// 停止扫描
+    func stop() {
+        stopRunning()
+    }
+    
+    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
+        if (metadataObjects.count > 0) {
+            // 停止扫描
+            stop()
+            playSound()
+            // 获取信息
+            let result = metadataObjects.last as! AVMetadataMachineReadableCodeObject
+            block!(result.stringValue)
+        }
+    }
+}
+
+enum AVCaptureType {
+    case AVCaptureTypeQRCode
+    case AVCaptureTypeBarCode
+    case AVCaptureTypeBoth
+    func supportTypes() -> [String] {
+        switch self {
+        case .AVCaptureTypeQRCode:
+            return [AVMetadataObjectTypeQRCode]
+        case .AVCaptureTypeBarCode:
+            return [AVMetadataObjectTypeDataMatrixCode,
+                    AVMetadataObjectTypeITF14Code,
+                    AVMetadataObjectTypeInterleaved2of5Code,
+                    AVMetadataObjectTypeAztecCode,
+                    AVMetadataObjectTypePDF417Code,
+                    AVMetadataObjectTypeCode128Code,
+                    AVMetadataObjectTypeCode93Code,
+                    AVMetadataObjectTypeEAN8Code,
+                    AVMetadataObjectTypeEAN13Code,
+                    AVMetadataObjectTypeCode39Mod43Code,
+                    AVMetadataObjectTypeCode39Code,
+                    AVMetadataObjectTypeUPCECode]
+        case .AVCaptureTypeBoth:
+            return [AVMetadataObjectTypeQRCode,
+                    AVMetadataObjectTypeDataMatrixCode,
+                    AVMetadataObjectTypeITF14Code,
+                    AVMetadataObjectTypeInterleaved2of5Code,
+                    AVMetadataObjectTypeAztecCode,
+                    AVMetadataObjectTypePDF417Code,
+                    AVMetadataObjectTypeCode128Code,
+                    AVMetadataObjectTypeCode93Code,
+                    AVMetadataObjectTypeEAN8Code,
+                    AVMetadataObjectTypeEAN13Code,
+                    AVMetadataObjectTypeCode39Mod43Code,
+                    AVMetadataObjectTypeCode39Code,
+                    AVMetadataObjectTypeUPCECode]
+        }
+    }
+}
