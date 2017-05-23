8a39c520105b1de0ff249eecdc5d6e04758b80e3
zhang28602 <zeyu930213@gmail.com>
2017-05-24 01:48:47 +0800

    完成扫描demo

--- ZZYQRCodeSwift/ScanViewController.swift
+++ ZZYQRCodeSwift/ScanViewController.swift
@@ -8,17 +8,72 @@
 
 import UIKit
 
-class ScanViewController: UIViewController {
+class ScanViewController: UIViewController ,UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     var sessionManager:AVCaptureSessionManager?
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         sessionManager = AVCaptureSessionManager(captureType: .AVCaptureTypeBoth, scanRect: CGRect.null, success: { (result) in
             if let r = result {
-                print(r)
+                self .showResult(result: r)
             }
         })
         sessionManager?.showPreViewLayerIn(view: view)
         sessionManager?.isPlaySound = true
+        
+        let item = UIBarButtonItem(title: "相册", style: .plain, target: self, action: #selector(openPhotoLib))
+        navigationItem.rightBarButtonItem = item
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        sessionManager?.start()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        sessionManager?.stop()
+    }
+    
+    func showResult(result: String) {
+        let alert = UIAlertView(title: "扫描结果", message: result, delegate: nil, cancelButtonTitle: "确定")
+        alert .show()
+    }
+    
+    func openPhotoLib() {
+        AVCaptureSessionManager.checkAuthorizationStatusForPhotoLibrary(grant: { 
+            let imagePicker = UIImagePickerController()
+            imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+            imagePicker.delegate = self
+            self.present(imagePicker, animated: true, completion: nil)
+        }) { 
+            let action = UIAlertAction(title: "确定", style: UIAlertActionStyle.default, handler: { (action) in
+                let url = URL(string: UIApplicationOpenSettingsURLString)
+                UIApplication.shared.openURL(url!)
+            })
+            let con = UIAlertController(title: "权限未开启",
+                                        message: "您未开启相册权限，点击确定跳转至系统设置开启",
+                                        preferredStyle: UIAlertControllerStyle.alert)
+            con.addAction(action)
+            self.present(con, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        sessionManager?.start()
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        dismiss(animated: true) { 
+            self.sessionManager?.start()
+            self.sessionManager?.scanPhoto(image: info["UIImagePickerControllerOriginalImage"] as! UIImage, success: { (str) in
+                if let result = str {
+                    self.showResult(result: result)
+                }else {
+                    self.showResult(result: "未识别到二维码")
+                }
+            })
+            
+        }
     }
 
 
--- ZZYQRCodeSwift/ViewController.swift
+++ ZZYQRCodeSwift/ViewController.swift
@@ -14,6 +14,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
 
+    @IBAction func scanBtnClick(_ sender: Any) {
+        
+        AVCaptureSessionManager.checkAuthorizationStatusForCamera(grant: { 
+            let controller = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "scanViewController")
+            self.navigationController?.pushViewController(controller, animated: true)
+        }){
+            let action = UIAlertAction(title: "确定", style: UIAlertActionStyle.default, handler: { (action) in
+                let url = URL(string: UIApplicationOpenSettingsURLString)
+                UIApplication.shared.openURL(url!)
+            })
+            let con = UIAlertController(title: "权限未开启", message: "您未开启相机权限，点击确定跳转至系统设置开启", preferredStyle: UIAlertControllerStyle.alert)
+            con.addAction(action)
+            self.present(con, animated: true, completion: nil)
+        }
+    }
 
 
 
