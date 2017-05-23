40d9f8cb7a8f18dcf213e4f208f00529f6a42060
zhang28602 <zeyu930213@gmail.com>
2017-05-23 17:06:29 +0800

    添加创建二维码Demo

---
+++ ZZYQRCodeSwift/CreateViewController.swift
@@ -0,0 +1,38 @@
+//
+//  CreateViewController.swift
+//  ZZYQRCodeSwift
+//
+//  Created by 张泽宇 on 2017/5/23.
+//  Copyright © 2017年 zzy. All rights reserved.
+//
+
+import UIKit
+
+class CreateViewController: UIViewController {
+
+    @IBOutlet weak var QRImage: UIImageView!
+    @IBOutlet weak var QRImageBg: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let image = UIImage(named: "bigMax")
+        QRImage.image = UIImage.createCustomizeQRCode(size: 200,
+                                                      dataStr: "hello",
+                                                      imageType: .CircularImage,
+                                                      iconImage: image!,
+                                                      iconImageSize: 40)
+        let temp = UIImage.createCustomizeQRCode(size: 150,
+                                                 dataStr: "hello",
+                                                 imageType: .SquareImage,
+                                                 iconImage: image!,
+                                                 iconImageSize: 40);
+        let bgImage = UIImage(named: "flower")
+        QRImageBg.image = UIImage.addQRCodeBg(bgImage: bgImage!, bgImageSize: 200, QRImage: temp!)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
--- ZZYQRCodeSwift/ViewController.swift
+++ ZZYQRCodeSwift/ViewController.swift
@@ -10,17 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        let image = UIImage.createQRCode(size: 200, dataStr: "你好")
-        self.imageView.image = image; 
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
 
 }
